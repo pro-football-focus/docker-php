5ca97193ecf2d71e8e569ecb873fb8abedbfc311
Brad Condo <bradcondo@gmail.com>
2017-08-21 15:21:53 -0400

    Generalized PHP container to avoid in-app configuration

--- docker-app.sh
+++ docker-app.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
 
+# Check if we need to load secrets from S3 into the environment
+if [[ -n "$SECRETS_FILE" ]]; then
+  echo "---- Loading S3 secrets into environment from ${SECRETS_FILE} ----"
+  eval $(aws s3 cp s3://${SECRETS_FILE} - --region ${AWS_DEFAULT_REGION} | sed 's/^/export /')
+else
+  echo "---- No secrets file specified, ignoring ----"
+fi
+
+# Output Version
+if [[ -f "version" ]]; then
+  VERSION=$(cat version)
+  echo "---- VERSION: ${VERSION} ----"
+fi
+
 # Setup and start the Apache+PHP application
 source /etc/apache2/envvars
 exec apache2 -D FOREGROUND
